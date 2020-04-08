cd8886672037137802aff41775d773bdce42eb9a
diyi <diyi@wacai.com>
2020-04-08 10:04:58 +0800

    fix grpc case

--- pkg/grpc/greeter_client/main.go
+++ pkg/grpc/greeter_client/main.go
@@ -25,8 +25,8 @@ import (
 	"os"
 	"time"
 
+	pb "github.com/xutao1989103/first-go-app/pkg/grpc/helloworld"
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
